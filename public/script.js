13e46ad06a2c9db82002c481fd65a60581a1a58e
Glitch (ml-sol-03) <none>
2021-02-05 09:11:31 +0000

    🏤🏏 Updated with Glitch

--- public/script.js
+++ public/script.js
@@ -1,15 +1,15 @@
 /* If you're feeling fancy you can add interactivity 
     to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
+// prints "Hello from the Web App Dev 1 lab!" in the browser's dev tools console
 console.log("Hello from the Web App Dev 1 lab!");
 
-function likeIt(){
-  alert('Thanks! You\'re okay too');
+function likeIt() {
+  alert("Thanks! You're okay too");
 }
 
 function showHide() {
-  var readMoreDiv = document.getElementById("readmore");
+  let readMoreDiv = document.getElementById("readmore");
   readMoreDiv.style.color = "green";
   if (readMoreDiv.style.display === "block") {
     readMoreDiv.style.display = "none";
@@ -19,10 +19,13 @@ function showHide() {
 }
 
 function welcomeUser() {
-  var username = prompt("What's your name?");
-  var welcomeUserDiv = document.getElementById("welcomeuser");
+  let username = prompt("What's your name?");
+  let welcomeUserDiv = document.getElementById("welcomeuser");
   welcomeUserDiv.style.display = "block";
-  document.getElementById('welcomeuser').innerHTML = '<p> Hello, ' + username + ', looking forward to hearing your playlists! Click this message to close it.</p>';
+  document.getElementById("welcomeuser").innerHTML =
+    "<p> Hello, " +
+    username +
+    ", looking forward to hearing your playlists! Click this message to close it.</p>";
   welcomeUserDiv.style.cursor = "pointer";
 }
 
@@ -34,53 +37,62 @@ function hideWelcome() {
 }
 
 const sonatas = {
-  title: 'Beethoven Sonatas',
+  title: "Beethoven Sonatas",
   songs: [
     {
-      title: 'Piano Sonata No. 3',
-      artist: 'Beethoven',
+      title: "Piano Sonata No. 3",
+      artist: "Beethoven"
     },
     {
-      title: 'Piano Sonata No. 7',
-      artist: 'Beethoven',
+      title: "Piano Sonata No. 7",
+      artist: "Beethoven"
     },
     {
-      title: 'Piano Sonata No. 10',
-      artist: 'Beethoven',
+      title: "Piano Sonata No. 10",
+      artist: "Beethoven"
     },
     {
-      title: 'Piano Sonata No. 13',
-      artist: 'Beethoven',
+      title: "Piano Sonata No. 13",
+      artist: "Beethoven"
     }
   ],
   getRating() {
-    let userRating = parseInt(prompt("Rate this collection (from 1 to 5 stars)"));
-    if (userRating>5 || userRating<1 || isNaN(userRating)){
+    let userRating = parseInt(
+      prompt("Rate this collection (from 1 to 5 stars)")
+    );
+    if (userRating > 5 || userRating < 1 || isNaN(userRating)) {
       alert("Try again with a number between 1 and 5!");
-    }
-    else{
+    } else {
       $("#rating").html("You gave a rating of: ");
-      for (let i=0; i < userRating; i++){
-          $("#rating").append("<i class='yellow star icon'></i>");
+      for (let i = 0; i < userRating; i++) {
+        $("#rating").append("<i class='yellow star icon'></i>");
       }
     }
   }
 };
 
-$(document).ready(function(){
-
+$(document).ready(function() {
   $("#tableoutput").html("<h2 class='ui header'>" + sonatas.title + "</h2>");
 
-  $("#tableoutput").append("<table class='ui fixed striped table'><thead><tr><th>Song</th><th>Artist</th></thead><tbody>");
+  $("#tableoutput").append(
+    "<table class='ui fixed striped table'><thead><tr><th>Song</th><th>Artist</th></tr></thead><tbody>"
+  );
 
-  for (let i in sonatas.songs){
-        $("#tableoutput tr:last").after("<tr><td>" + sonatas.songs[i].title + "</td><td>" + sonatas.songs[i].artist + "</td></tr>");
+  for (let i in sonatas.songs) {
+    $("#tableoutput tr:last").after(
+      "<tr><td>" +
+        sonatas.songs[i].title +
+        "</td><td>" +
+        sonatas.songs[i].artist +
+        "</td></tr>"
+    );
   }
 
   $("#tableoutput").append("</tbody></table>");
-  
-  $("#tableoutput").append('<p><span id="rating"> </span></p>');
-  
-  $("#tableoutput").append('<button class="ui blue button" onclick="sonatas.getRating()"> Rate it! <i class="star icon"></i></button>');
 
+  $("#tableoutput").append('<p><span id="rating"> </span></p>');
+
+  $("#tableoutput").append(
+    '<button class="ui blue button" onclick="sonatas.getRating()"> Rate it! <i class="star icon"></i></button>'
+  );
 });
